a6cd5aadc205c5c94d8d5ce1d093e94a42b790cf
Trevor Kim <trevorkim3141@gmail.com>
2025-06-28 11:44:11 -0400

    mac compatible

---
+++ bits.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_render.h>
-#include <bits/stdc++.h>
+#include "bits.h"
 #include <cstddef>
 #include <cstdint>
 
